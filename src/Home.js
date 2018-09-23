94c420a013f965e86a9076b3ac482fd0b3673d47
rajeshthangaraj1 <rajeshthangaraj1@gmail.com>
2018-09-23 21:25:56 +0400

    first comments
    first comments

--- src/Home.js
+++ src/Home.js
@@ -1,28 +1,31 @@
 import React from 'react';
+import DocumentMeta from 'react-document-meta';
+const lang = require('./lang');
 
+/* Load meta tags */
 
-//import {geolocated} from 'react-geolocated';
-
-import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
+const meta = {
+  title: lang.home_meta_title,
+  description: lang.home_meta_title,
+  meta: {
+	name: {
+	  keywords: lang.home_meta_title
+	}
+  }
+};
 class Home extends React.Component {
+	
   render() {
     return (
-      <Map google={this.props.google} zoom={14}>
- 
-        <Marker onClick={this.onMarkerClick}
-                name={'Current location'} />
- 
-        <InfoWindow onClose={this.onInfoWindowClose}>
-            <div>
-              <h1>{this.state.selectedPlace.name}</h1>
-            </div>
-        </InfoWindow>
-      </Map>
+    
+    <div>   	<DocumentMeta  {...meta} />
+    
+      <div id="maps" style={{position: ''}}></div>
+      <div id="button-layer"><button id="btnAction" style={{display: 'none'}}>My Current Location</button></div>
+    </div>
+    
     );
   }
 }
- 
-export default GoogleApiWrapper({
-  apiKey: ('AIzaSyA-AB-9XZd-iQby-bNLYPFyb0pR2Qw3orw')
-})(Home)
+export default Home;
 
